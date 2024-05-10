c4c48a174ce6d323bcbb64d7d8898ff2c48b98db
mohamed hussein <mh95568@gmail.com>
2024-05-10 18:39:56 +0300

    MainScreen Ui

---
+++ SportSync/SportSync/MainScreenCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MainScreenCollectionViewCell.swift
+//  SportSync
+//
+//  Created by Hussien on 10/05/2024.
+//
+
+import UIKit
+
+class MainScreenCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var mainTitle: UILabel!
+    @IBOutlet weak var mainImage: UIImageView!
+    
+}
