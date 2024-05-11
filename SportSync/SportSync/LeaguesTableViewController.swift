cbe09be242bc30d9d6a2c08e2764d0de7faf3fcf
mohamed hussein <mh95568@gmail.com>
2024-05-11 13:20:26 +0300

    Leagues table Done

--- SportSync/SportSync/LeaguesTableViewController.swift
+++ SportSync/SportSync/LeaguesTableViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class LeaguesTableViewController: UITableViewController {
-
+    let names :[String] = ["mohamed" , "ahmed","anas"]
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,7 +20,7 @@ class LeaguesTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        return names.count
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -29,7 +29,7 @@ class LeaguesTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 10
+        return 1
     }
 
     
@@ -37,6 +37,8 @@ class LeaguesTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "LeaguesTableViewCell", for: indexPath) as! LeaguesTableViewCell
         
         cell.leagueImage.image = UIImage(named: "tom-briskey-HM3WZ4B1gvM-unsplash")
+        
+        cell.leagueTitle.text = names[indexPath.section]
         
 
          
